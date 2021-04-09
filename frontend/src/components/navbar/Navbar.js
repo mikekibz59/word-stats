1e9ed2254cc9872c412486eb2acc940253e3b9e8
mikekibz59 <mikekibz59@gmail.com>
2021-04-10 00:57:53 +0300

    Added Forms, Header, Navbar components

---
+++ frontend/src/components/navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Navbar.css'
+
+export default function Navbar(){
+  return (
+    <nav className="nav" id="menu">
+      <div className="wrap">
+        <div className="brand">
+          <span>Stats</span>
+        </div>
+      </div>
+    </nav>
+  )
+} 
