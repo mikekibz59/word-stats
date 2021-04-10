efd907d621463807bae9de570c42c3373cb9e94f
mikekibz59 <mikekibz59@gmail.com>
2021-04-10 16:33:19 +0300

    Added react router and React context for state management

--- frontend/src/components/app/App.js
+++ frontend/src/components/app/App.js
@@ -1,14 +1,35 @@
-import './App.css';
-import Navbar from '../navbar/Navbar'
-import Header from '../header/Header'
+/** @format */
 
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import './App.css';
+import Navbar from '../navbar/Navbar';
+import Header from '../header/Header';
+import StatsContext from './StatsContext.js';
 function App() {
-  return (
-    <div className="App">
-        <Navbar/>
-        <Header/>
-    </div>
-  );
+	const [ngram, setNgram] = useState(1);
+	const [body, setBody] = useState('');
+	const [case_sensitive, setSensitivity] = useState(false);
+	const [length, setLength] = useState(100);
+	const state = {
+		ngram,
+		setNgram,
+		body,
+		setBody,
+		case_sensitive,
+		setSensitivity,
+		length,
+		setLength,
+	};
+	return (
+		<Router>
+			<div className='App'>
+				<StatsContext.Provider value={state}>
+					<Navbar />
+					<Header />
+				</StatsContext.Provider>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
