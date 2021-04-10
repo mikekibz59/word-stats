8bbd2a6794f9e3f569f2c726998fefcd3c2648e9
mikekibz59 <mikekibz59@gmail.com>
2021-04-10 14:35:49 +0300

    Added styles to header component Added some temporary styles to the form added a submit callback to hand form submission

--- frontend/src/components/form/Form.js
+++ frontend/src/components/form/Form.js
@@ -1,51 +1,59 @@
-import React, {useState} from 'react'
+/** @format */
 
-export default function Form(){
-  const [ngram, setNgram] = useState(1)
-  const [body, setBody] = useState('')
-  const [case_sensitive, setSensitivity] = useState(false)
-  const [length, setLength] = useState(100)
-  const submit = e => {
-    e.preventDefault()
-    fetch(`http://localhost:8000/api/stats/create_stats`, {
-      method: 'POST',
-      body: JSON.stringify({ ngram, body, case_sensitive, length }),
-    })
-  }
-  return (
-    <form>
-      <label htmlFor="body">Text</label>
-      <textarea
-        name="body"
-        value={body}
-        onChange={e => setBody(e.target.value)}
-        />
-      <br />
-      <label htmlFor="ngam"> ngram</label> <br />
-      <input
-        name="ngram"
-        value={ngram}
-        onChange={e => setNgram(e.target.value)}
-       />
-      <br />
+import React, { useState } from 'react';
+import axios from 'axios';
+import './Form.css';
 
-      <label htmlFor="case sensitive"> case sensitive</label> <br />
-      <input
-        type="radio"
-        name="case_sensitive"
-        value={case_sensitive}
-        onChange={e => setSensitivity(e.target.value)}
-       />
-      <br />
+export default function Form() {
+	const [ngram, setNgram] = useState(1);
+	const [body, setBody] = useState('');
+	const [case_sensitive, setSensitivity] = useState(false);
+	const [length, setLength] = useState(100);
+	const submit = (e) => {
+		e.preventDefault();
+		let payload = { ngram, body, case_sensitive, length };
+		console.log(payload);
+		axios
+			.post('http://localhost:8000/api/stats/create_stats', payload)
+			.then((res) => {
+				console.log(res);
+				console.log(res.data);
+			});
+	};
 
-      <label htmlFor="length"> length</label> <br />
-      <input
-        name="length"
-        value={length}
-        onChange={e => setLength(e.target.value)}
-       />
-      <br />
-      <button type="submit">Send it!</button>
-    </form>
-  )
+	return (
+		<form className='stats_form'>
+			<label htmlFor='ngam'> ngram</label> <br />
+			<input
+				name='ngram'
+				value={ngram}
+				onChange={(e) => setNgram(e.target.value)}
+			/>
+			<br />
+			<label htmlFor='length'> length</label> <br />
+			<input
+				name='length'
+				value={length}
+				onChange={(e) => setLength(e.target.value)}
+			/>
+			<br />
+			<label htmlFor='body'>Text</label>
+			<textarea
+				name='body'
+				value={body}
+				onChange={(e) => setBody(e.target.value)}
+			/>{' '}
+			<br />
+      <label htmlFor='case sensitive'> case sensitive</label> <br />
+			<input
+				className='switch'
+				type='checkbox'
+				value={case_sensitive}
+				onChange={(e) => setSensitivity((prev) => !prev)}
+			/>
+			<button type='submit' onClick={submit}>
+				Send it!
+			</button>
+		</form>
+	);
 }
--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -1,20 +1,26 @@
-import React from 'react'
-import Form from '../form/Form'
-import "./Header.css"
+/** @format */
 
-export default function Header(){
-  return (
-    <header className="hero">
-      <div className="content">
-        <div className="placeholder">
-          <div>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse cumque, placeat in.</p>
-          </div>
-          <div>
-            <Form/>
-          </div>
-        </div>
-      </div>
-    </header>
-  )
+import React from 'react';
+import Form from '../form/Form';
+import './Header.css';
+
+export default function Header() {
+	return (
+		<header className='hero'>
+			<div className='content'>
+				<div className='wrapper'>
+					<div className='wrapper__item'>
+						<h1>WordStats calculator</h1>
+						<p>
+							Get specialized statistic on the text that type <br />
+							exactly the same way a machine may understand
+						</p>
+					</div>
+					<div className='wrapper__item'>
+						<Form />
+					</div>
+				</div>
+			</div>
+		</header>
+	);
 }
