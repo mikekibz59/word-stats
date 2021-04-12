77b386d6d7cc1bba841836e90e5f82842fbfd5c8
mikekibz59 <mikekibz59@gmail.com>
2021-04-13 01:05:21 +0300

    reformated styles and made results page responsive

--- frontend/src/components/charts/BarChart.js
+++ frontend/src/components/charts/BarChart.js
@@ -1,15 +1,22 @@
 /** @format */
 
 import BarChartConfig from './BarChartConfg';
+import { Theme } from './BarChartTheme';
 import React, { useContext } from 'react';
-import ReactHighCharts from 'react-highcharts';
+import Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
 import { StatsContext } from '../app/StatsContext';
+import './Chart.css';
 
+Highcharts.setOptions(Theme);
 export default function () {
 	const { result } = useContext(StatsContext);
 	return (
-		<div>
-			<ReactHighCharts config={BarChartConfig(result)} />
+		<div className='chart-div'>
+			<HighchartsReact
+				highcharts={Highcharts}
+				options={BarChartConfig(result)}
+			/>
 		</div>
 	);
 }
--- frontend/src/components/navbar/Navbar.js
+++ frontend/src/components/navbar/Navbar.js
@@ -3,27 +3,50 @@
 import React, { useContext } from 'react';
 import './Navbar.css';
 import { StatsContext } from '../app/StatsContext';
+import { isEmpty } from 'lodash';
 
-export default function Navbar({ setViewResult }) {
-	const { result } = useContext(StatsContext);
+const ChangeView = ({ result, viewResult, setViewResult }) => {
+	const result_ = isEmpty(result);
+	console.log('result', result_);
+	console.log('viewresult', viewResult)
+	if (!result_ || !viewResult) {
+		return (
+			<div
+				className='action_btn'
+				onClick={() => {
+					setViewResult(true);
+				}}>
+				view results
+			</div>
+		);
+	} else if (!result_ && viewResult) {
+		return (
+			<div
+				className='action_btn'
+				onClick={() => {
+					setViewResult(false);
+				}}>
+				back
+			</div>
+		);
+	}
+	else{
+		return ''
+	}
+};
+export default function Navbar() {
+	const { result, viewResult, setViewResult } = useContext(StatsContext);
+	console.log(result);
 	return (
 		<nav className='nav' id='menu'>
 			<div className='wrap'>
 				<div className='brand'>
 					<span>Stats</span>
-				</div>
-				<div className='action_btn'>
-					{Object.keys(result) === 0 ? (
-						<button
-							onClick={() => {
-								setViewResult((prevView) => !prevView);
-							}}>
-							{' '}
-							View results
-						</button>
-					) : (
-						''
-					)}
+					<ChangeView
+						result={result}
+						viewResult={viewResult}
+						setViewResult={setViewResult}
+					/>
 				</div>
 			</div>
 		</nav>
